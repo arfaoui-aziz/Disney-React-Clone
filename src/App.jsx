2a796ed612123124906b90c337b09d9eac8a8dc9
arfaoui-aziz <aziz.arfaou.98@gmail.com>
2021-09-09 19:14:50 +0100

    feat: Adding Movie Detail Component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Login from "./components/Login";
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Detail from "./components/Detail";
 
 function App() {
   return (
@@ -18,6 +19,9 @@ function App() {
           </Route>
           <Route path="/home">
             <Home />
+          </Route>
+          <Route path="/detail/:id">
+            <Detail />
           </Route>
         </Switch>
       </Router>
